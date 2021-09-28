59b60da2ada6d6f63c8c5529a2fe99b1c40e105d
dennis-199 <otieno.dennis@strathmore.edu>
2021-09-28 21:06:35 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+myList = ["David","Daisy","Lynn","Lavender","Susan"]
+print(myList)
+print(len(myList))
+print(myList[3])
+print(myList[len(myList)-1])
+print(myList[-3])
